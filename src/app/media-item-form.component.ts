b51c8b0dc51c811b35f99d91b804d196ee6eada8
twknab <20636750+twknab@users.noreply.github.com>
2023-05-13 16:45:31 -0700

    swap FormGroup for FormBuilder

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from "@angular/forms";
 
 @Component({
   selector: "media-item-form",
@@ -9,19 +14,21 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 export class MediaItemFormComponent implements OnInit {
   form: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
   ngOnInit() {
     // Use form groups and form controls to define the models you'll be using
-    this.form = new FormGroup({
-      medium: new FormControl("Movies"),
-      name: new FormControl(
+    this.form = this.formBuilder.group({
+      medium: this.formBuilder.control("Movies"),
+      name: this.formBuilder.control(
         "",
         Validators.compose([
           Validators.required,
           Validators.pattern("[\\w\\-\\s\\/]+"),
         ])
       ),
-      category: new FormControl(""),
-      year: new FormControl("", this.yearValidator),
+      category: this.formBuilder.control(""),
+      year: this.formBuilder.control("", this.yearValidator),
     });
   }
 
