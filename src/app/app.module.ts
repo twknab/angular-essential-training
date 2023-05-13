1be6b6c66a390fe6b604d1fd7ea40fa62b80c0a3
twknab <20636750+twknab@users.noreply.github.com>
2023-05-12 18:25:37 -0700

    add media item component to app layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
 
 @NgModule({
   imports: [
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   exports: [],
   declarations: [
     AppComponent,
+    MediaItemComponent,
   ], // components go here
   bootstrap: [
     AppComponent,
