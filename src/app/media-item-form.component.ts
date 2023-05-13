ebb13107f08c321afc5fecbd7322dd1b56cfeb33
twknab <20636750+twknab@users.noreply.github.com>
2023-05-13 00:31:15 -0700

    add frontend validations

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "media-item-form",
@@ -10,9 +10,16 @@ export class MediaItemFormComponent implements OnInit {
   form: FormGroup;
 
   ngOnInit() {
+    // Use form groups and form controls to define the models you'll be using
     this.form = new FormGroup({
       medium: new FormControl("Movies"),
-      name: new FormControl(""),
+      name: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("[\\w\\-\\s\\/]+"),
+        ])
+      ),
       category: new FormControl(""),
       year: new FormControl(""),
     });
