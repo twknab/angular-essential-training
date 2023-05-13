d0ba9fd365b40309bd7e66b0db4e5d462623c0ee
twknab <20636750+twknab@users.noreply.github.com>
2023-05-12 18:22:41 -0700

    add media component

---
+++ src/app/media-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'media-item',
+  templateUrl: 'media-item.component.html',
+  styleUrls: ['./media-item.component.scss',],
+})
+
+export class MediaItemComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
