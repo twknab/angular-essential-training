5f91148394d30dca35c291aa1127740d1c99797d
twknab <20636750+twknab@users.noreply.github.com>
2023-05-13 00:45:29 -0700

    add frontend errors text

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -21,8 +21,28 @@ export class MediaItemFormComponent implements OnInit {
         ])
       ),
       category: new FormControl(""),
-      year: new FormControl(""),
+      year: new FormControl("", this.yearValidator),
     });
+  }
+
+  // Custom validator
+  yearValidator(control: FormControl) {
+    if (control.value.trim().length === 0) {
+      return null;
+    }
+    const year = parseInt(control.value, 10);
+    const minYear = 1900;
+    const maxYear = 2100;
+    if (year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return {
+        year: {
+          min: minYear,
+          max: maxYear,
+        },
+      };
+    }
   }
 
   onSubmit(mediaItem) {
