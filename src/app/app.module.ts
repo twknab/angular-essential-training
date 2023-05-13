818c61adf8393be1887ef56aa82efd0012c69757
twknab <20636750+twknab@users.noreply.github.com>
2023-05-13 00:25:56 -0700

    swapped ngModel for FormGroup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { MediaItemComponent } from "./media-item.component";
 import { MediaItemFormComponent } from "./media-item-form.component";
@@ -9,7 +9,7 @@ import { FavoriteDirective } from "./favorite.directive";
 import { CategoryListPipe } from "./category-list.pipe";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, ReactiveFormsModule],
   exports: [],
   declarations: [
     AppComponent,
