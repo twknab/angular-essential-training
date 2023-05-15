2fa1c69b071a207645030c5631296afa549e905b
twknab <20636750+twknab@users.noreply.github.com>
2023-05-15 04:44:03 -0700

    add http get/post/delete methods

--- src/app/media-item.service.ts
+++ src/app/media-item.service.ts
@@ -1,59 +1,11 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { map } from "rxjs/operators";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { map, catchError } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root", // This will allow our service to be accessible throughout whole application
 })
 export class MediaItemService {
-  mediaItems = [
-    {
-      id: 1,
-      name: "Firebug",
-      medium: "Series",
-      category: "Science Fiction",
-      year: 2010,
-      watchedOn: 1659166565384,
-      isFavorite: false,
-    },
-    {
-      id: 2,
-      name: "The Small Tall",
-      medium: "Movies",
-      category: "Comedy",
-      year: 2015,
-      watchedOn: null,
-      isFavorite: true,
-    },
-    {
-      id: 3,
-      name: "The Redemption",
-      medium: "Movies",
-      category: "Action",
-      year: 2016,
-      watchedOn: null,
-      isFavorite: false,
-    },
-    {
-      id: 4,
-      name: "Hoopers",
-      medium: "Series",
-      category: "Drama",
-      year: null,
-      watchedOn: null,
-      isFavorite: true,
-    },
-    {
-      id: 5,
-      name: "Happy Joe: Cheery Road",
-      medium: "Movies",
-      category: "Action",
-      year: 2015,
-      watchedOn: 1457166565384,
-      isFavorite: false,
-    },
-  ];
-
   constructor(private http: HttpClient) {}
 
   // This will be on interview
@@ -64,19 +16,21 @@ export class MediaItemService {
     return this.http.get<MediaItemResponse>("mediaitems", getOptions).pipe(
       map((response) => {
         return response.mediaItems;
-      })
+      }),
+      catchError(this.handleError)
     );
   }
 
   add(mediaItem) {
-    this.mediaItems.push(mediaItem);
+    return this.http.post("mediaitems", mediaItem);
   }
 
   delete(mediaItem) {
-    const index = this.mediaItems.indexOf(mediaItem);
-    if (index >= 0) {
-      this.mediaItems.splice(index, 1);
-    }
+    return this.http.delete(`mediaitems/${mediaItem.id}`);
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+    console.log(error.message);
   }
 }
 
