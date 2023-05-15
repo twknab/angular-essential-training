58f9aeff73fd0939b43648dc4b0a240df5e6bd76
twknab <20636750+twknab@users.noreply.github.com>
2023-05-15 05:22:16 -0700

    wire up nav

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { MediaItemListComponent } from "./media-item-list.component";
 
 const appRoutes: Routes = [
   { path: "add", component: MediaItemFormComponent },
+  // adding the colon here will actually pass that data to the component which can be used
   { path: ":medium", component: MediaItemListComponent },
   { path: "", redirectTo: "all", pathMatch: "full" },
 ];
